8baa8ae410e24ab94323fd5236012970d89067b9
Manasi Rawas <71628834+ManasiRawas@users.noreply.github.com>
2026-07-10 15:43:13 +0530

    Use environment variable for backend API

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -40,9 +40,12 @@ export default function Home() {
     setLoading(true);
 
     try {
-      const response = await fetch(
-        "http://127.0.0.1:8000/generate-outfit",
-        {
+      const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+
+const response = await fetch(
+  `${API_URL}/generate-outfit`,
+  {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
